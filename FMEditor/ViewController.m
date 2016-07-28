2f0e85f37f99353d564c59f1dccf797b17db648a
mc <mc@scorpion.local>
2016-07-28 16:21:05 +0800

    Initial Commit

---
+++ FMEditor/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FMEditor
+//
+//  Created by mc on 16/7/28.
+//  Copyright © 2016年 Scorpion. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
